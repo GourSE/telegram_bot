033d5d0e3e94bec56d5b251d3aab8091411eb595
Edward Edwards <Sebastian.Edwards@tuta.io>
2020-08-05 20:47:02 +0800

    telegram api controler

---
+++ core.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+
+import requests
+import json
+import configparser as cfg
+
+
+class telegram_bot():
+
+    def __init__(self, config):
+        self.token = self.read_token_from_config_file(config)
+        self.base = "https://api.telegram.org/bot{}/".format(self.token)
+
+    def get_updates(self, offset=None):
+        url = self.base + "getUpdates?timeout=10"
+        if offset:
+            url = url + "&offset={}".format(offset + 1)
+        r = requests.get(url)
+        return json.loads(r.content)
+
+    def send_message_markdown(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id={}&text={}&parse_mode=MarkdownV2".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
+
+    def send_message(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
+    
+    def reply_message(self, msg, chat_id, message_id):
+        url = self.base + "sendMessage?chat_id={}&text={}&reply_to_message_id={}&parse_mode=MarkdownV2".format(chat_id, msg, message_id)
+        if msg is not None:
+            requests.get(url)
+
+    def read_token_from_config_file(self, config):
+        parser = cfg.ConfigParser()
+        parser.read(config)
+        return parser.get('creds', 'token')
+    
+    def get_me(self):
+        url = self.base + "getMe"
+        r = requests.get(url)
+        return json.loads(r.content)
+        
