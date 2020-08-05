8bc2d95793a6069df974df0a1d823a839183913f
Edward Edwards <Sebastian.Edwards@tuta.io>
2020-08-05 21:12:56 +0800

    Update core.py

--- core.py
+++ core.py
@@ -11,13 +11,6 @@ class telegram_bot():
         self.token = self.read_token_from_config_file(config)
         self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
-    def get_updates(self, offset=None):
-        url = self.base + "getUpdates?timeout=10"
-        if offset:
-            url = url + "&offset={}".format(offset + 1)
-        r = requests.get(url)
-        return json.loads(r.content)
-
     def send_message_markdown(self, msg, chat_id):
         url = self.base + "sendMessage?chat_id={}&text={}&parse_mode=MarkdownV2".format(chat_id, msg)
         if msg is not None:
