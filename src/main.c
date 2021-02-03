77a4001cc65381111696037a0d2c3b2b6206dbea
Pokemob <35000549+GourSE@users.noreply.github.com>
2021-02-03 16:33:14 +0800

    improve prompts and Makefile (#2)
    * improvements
    
    improve prompts in scripts
    add flags in main.c
    -f telegram_bot.py
    -p message_pusher.py
    -s support_bot.py
    
    * patch: locate file if binary in repo dir
    
    Use config file as a landmark for application in local, will find script in current directiory if none
    
    * update: edit config with argv
    
    flag: --config, -c
    Edit config.cfg, will edit config in repo dir if no file in local. Text editor will be nano.
    
    * tweaks: Makefile and edit README.md
    
    Makefile will check if config file exists in local, if true, will not replace.
    Updae README.md
    Clean comments in code
    
    * patch: colour in terminal output while make
    
    config command instruction has two command, only one was coloured
    
    * add binary in ignore

--- src/main.c
+++ src/main.c
@@ -20,19 +20,136 @@ const char SUPPORT_BOT[] = "python support_bot.py";
 
 #elif __linux__
 
+#define _PROGRAM_NAME "Telegram Bot"
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+const char TELEGRAM_BOT_C[] = "python3 telegram_bot.py";
+const char MESSAGE_PUSHER_C[] = "python3 message_pusher.py";
+const char SUPPORT_BOT_C[] = "python3 support_bot.py";
 
 const char TELEGRAM_BOT[] = "python3 ~/.local/share/telegram_bot/telegram_bot.py";
 const char MESSAGE_PUSHER[] = "python3 ~/.local/share/telegram_bot/message_pusher.py";
 const char SUPPORT_BOT[] = "python3 ~/.local/share/telegram_bot/support_bot.py";
 
+int getLocalConfig();
+
 #endif
 
 
-int main()
+int launchScript(int mode);
+
+
+int main(int argc, char **argv)
 {
+
+    int r;
+    char flg[50];
+
+    for (r = 0; r < argc; r++)
+    {
+        if (r == 0)
+        {
+            strcpy(flg, "*noFlag");
+        }
+        else if (r == 1)
+        {
+            strcpy(flg, argv[1]);
+        }
+        else if (r > 1)
+        {
+            printf("usage: telegram_bot <-f/-p/-s>\n\n");
+            return 1;
+        }
+    }
+
+    if (strcmp(flg, "-f") == 0)
+    {
+        printf("telegram bot\n\n");
+        launchScript(1);
+        return 0;
+    }        
+    else if (strcmp(flg, "-p") == 0)
+    {
+        printf("message pusher\n\n");
+        launchScript(2);
+        return 0;
+    }
+    else if (strcmp(flg, "-s") == 0)
+    {
+        printf("support bot\n\n");
+        launchScript(3);
+        return 0;
+    }
+    else if (strcmp(flg, "--config") == 0)
+    {
+        #ifdef __linux__
+        char command[150];
+
+        int built = getLocalConfig();
+
+        if (built == -1)
+        {
+            system("nano config.cfg");
+        }
+        else
+        {
+            char *homeDir = getenv("HOME");
+            char file_[] = "/.local/share/telegram_bot/config.cfg";
+            char *fileLoc = malloc(strlen(homeDir) + strlen(file_) + 1);
+            strcpy(fileLoc, homeDir);
+            strcat(fileLoc, file_);
+
+            sprintf(command, "%s %s", "nano", fileLoc);
+            system(command);
+        }
+        return 0;
+        #else
+        ;
+        #endif
+    }
+    else if (strcmp(flg, "-c") == 0)
+    {
+        #ifdef __linux__
+        char command[150];
+
+        int built = getLocalConfig();
+
+        if (built == -1)
+        {
+            system("nano config.cfg");
+        }
+        else
+        {
+            char *homeDir = getenv("HOME");
+            char file_[] = "/.local/share/telegram_bot/config.cfg";
+            char *fileLoc = malloc(strlen(homeDir) + strlen(file_) + 1);
+            strcpy(fileLoc, homeDir);
+            strcat(fileLoc, file_);
+
+            sprintf(command, "%s %s", "nano", fileLoc);
+            system(command);
+        }
+        return 0;
+        #else
+        ;
+        #endif
+    }
+    else if (strcmp(flg, "*noFlag") == 0)
+    {
+        ;
+    }
+    else
+    {
+        printf("'%s' not recognised\n", flg);
+        printf("usage: telegram_bot <-f/-p/-s>\n\n");
+
+        return 0;
+    }
+
     while(1)
     {
         char mode[30];
@@ -40,57 +157,149 @@ int main()
         scanf("%s", mode);
         switch (mode[0])
         {
-        case '1':
-            #ifdef _WIN32
-            system("cls");
-            #else
-            system("clear");
-            #endif
+            case '1':
+                #ifdef _WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif
 
-            printf("telegram bot selected\n\n");
-            system(TELEGRAM_BOT);
-            return 0;
-            break;
-        
-        case '2':
-            #ifdef _WIN32
-            system("cls");
-            #else
-            system("clear");
-            #endif
+                printf("telegram bot selected\n\n");
+                launchScript(1);
+                return 0;
+                break;
+            
+            case '2':
+                #ifdef _WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif
 
-            printf("message pusher selected\n\n");
-            system(MESSAGE_PUSHER);
-            return 0;
-            break;
+                printf("message pusher selected\n\n");
+                launchScript(2);
+                return 0;
+                break;
 
-        case '3':
-            #ifdef _WIN32
-            system("cls");
-            #else
-            system("clear");
-            #endif
+            case '3':
+                #ifdef _WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif
 
-            printf("support bot selected\n\n");
-            system(SUPPORT_BOT);
-            return 0;
-            break;
+                printf("support bot selected\n\n");
+                launchScript(3);
+                return 0;
+                break;
 
-        case 'q':
-            return 0;
+            case 'q':
+                return 0;
 
-        default:
-            printf("\n\n%s is not a mode\n", mode);
-            sleep(1);
+            default:
+                printf("\n\n%s is not a mode\n", mode);
+                sleep(1);
 
-            #ifdef _WIN32
-            system("cls");
-            #else
-            system("clear");
-            #endif
+                #ifdef _WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif
 
-            printf("%s is not a mode\n", mode);
-            break;
+                printf("%s is not a mode\n", mode);
+                break;
         }
     }
 }
+
+
+int launchScript(int mode)
+{
+    #ifdef __linux__
+
+    int built = getLocalConfig();
+
+    if (built == -1)
+    {
+        // debug
+        // printf("no\n");
+        
+        switch (mode)
+        {
+            case 1:
+                system(TELEGRAM_BOT_C);
+                return 0;
+            
+            case 2:
+                system(MESSAGE_PUSHER_C);
+                return 0;
+
+            case 3:
+                system(SUPPORT_BOT_C);
+                return 0;
+        }
+    }
+    else
+    {
+        switch (mode)
+        {
+            case 1:
+                system(TELEGRAM_BOT);
+                return 0;
+            
+            case 2:
+                system(MESSAGE_PUSHER);
+                return 0;
+
+            case 3:
+                system(SUPPORT_BOT);
+                return 0;
+        }
+    }
+    #else
+    switch (mode)
+    {
+        case 1:
+            system(TELEGRAM_BOT);
+            return 0;
+        
+        case 2:
+            system(MESSAGE_PUSHER);
+            return 0;
+
+        case 3:
+            system(SUPPORT_BOT);
+            return 0;
+    }
+    #endif
+    return 0;
+}
+
+
+#ifdef __linux__
+
+int getLocalConfig()
+{
+    char *homeDir = getenv("HOME");
+
+    char file_[] = "/.local/share/telegram_bot/config.cfg";
+
+    char *fileLoc = malloc(strlen(homeDir) + strlen(file_) + 1);
+    strcpy(fileLoc, homeDir);
+    strcat(fileLoc, file_);
+
+    struct stat buffer;
+    int file__ = stat(fileLoc, &buffer);
+
+    if (file__ == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    return 0;
+}
+
+#endif
